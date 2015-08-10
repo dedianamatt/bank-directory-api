8ea7fa3cc867f2f683e87c3de69fd94afafff6ee
Matt <dedianm@gmail.com>
2015-08-10 11:14:41 -0400

    Modified response object
    Parsing entire directory entry from text file and responding with the
    same.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # bank-directory-api
-HTTP API to Federal Reserve Bank Service's EPayments Directory written in Node.js. The directory comes from https://www.frbservices.org/EPaymentsDirectory/fpddir.txt. 
+Simple HTTP API to Federal Reserve Bank Service's EPayments Directory written in Node.js. The directory comes from https://www.frbservices.org/EPaymentsDirectory/fpddir.txt. 
 This file should be copied/replaced in this repository 
 
 - Download source and run npm install to install dependencies
@@ -7,15 +7,24 @@ This file should be copied/replaced in this repository
 
 App is deployed to heroku. Example HTTP GET:
 
-curl https://bank-directory-api.herokuapp.com/banks/211672683
+curl https://bank-directory-api.herokuapp.com/api/banks/211672683
 
 Example JSON Response:
-{"routingId":"211672683","bankName":"NORTHFIELD SAVINGS BANK"}
+{
+	"routingNumber":"211672683",
+	"telegraphicName":"NORTHFIELD SVGS VT",
+	"customerName":"NORTHFIELD SAVINGS BANK",
+	"state": "VT",
+	"city": "BARRE",
+	"fundsTransferStatus": true,
+	"fundsSettlementOnlyStatus": false,
+	"bookEntrySecuritiesTransferStatus": true,
+	"lastRevisedDate": ""
+}
 
 TODO:
 - Add route for all banks.
-- Add HTTP error responses
-- Parse out additional data from text Directory
+- Add request header or query for banks\:routingId route to reduce response size to routingNumber and bankName (customer name)
 - Add route for searches.
 - Add index.html 
 - Read fpddir.txt file from web instead of downloading it. 
--- server.js
+++ server.js
@@ -8,27 +8,42 @@ var server = server || {};
 /*****************/
 var app = express();
 
-app.get('/banks', function(req, res) {
+app.set('port', (process.env.PORT || 5000))
+
+app.get('/api/banks', function(req, res) {
 	console.log("all banks");
 	res.send();
 });
 
-app.get('/banks/:routingId', function(req, res) {
+app.get('/api/banks/:routingId', function(req, res) {
 	var inputId = req.params.routingId;
-	console.log(inputId);
-	if (typeof(server.routingIdMap[inputId]) !== 'undefined') {
-		var body = {'routingId':inputId, 'bankName':server.routingIdMap[inputId]};
-		res.json(body);
+	if (typeof(server.bankDirectory[inputId]) !== 'undefined') {		
+		res.json(server.bankDirectory[inputId]);
 	}
-	res.send();
+	else {
+		res.status(404).send("Sorry, we can't find a bank with that routing number");
+	}
 });
 
-app.listen(process.env.PORT || 1338, function() {
-	console.log("listening on port "+ process.env.PORT + " or port 1338");
+app.listen(app.get('port'), function() {
+	console.log("listening on port ", app.get('port'));
 	server.loadMap();
 });
 
-server.routingIdMap = {};
+/*server.bankDirectory is a map
+key: routingNumber,
+value: directoryEntry {
+	routingNumber: "this is the routingNumber",
+	telegraphicName: "internal bank name",
+	customerName: "human readable name for the bank",
+	state: "state or territory abbreviation in which bank is located",
+	city: "city in which bank is located",
+	fundsTransferStatus: "bool: true - Eligible, false - Ineligible",
+	fundsSettlementOnlyStatus: "bool: true - Settlement-only; false - not settlement-only",
+	bookEntrySecuritiesTransferStatus: "bool: true - eligible; false - ineligible",
+	lastRevisedDate: "date of last revision"
+} */
+server.bankDirectory = {};
 server.loadMap = function() {
 	fs.readFile('./fpddir.txt', "utf8", function(err, data) {
 		if (err) throw err;
@@ -38,8 +53,19 @@ server.loadMap = function() {
 			var routingId = bankLine.substring(0, 9).trim();
 			var bankName = bankLine.substring(27, 63).trim();
 			if (routingId.length > 0)
-				server.routingIdMap[routingId] = bankName;
+				server.bankDirectory[routingId] = {
+					routingNumber: routingId,
+					telegraphicName: bankLine.substring(9, 27).trim(),
+					customerName: bankLine.substring(27, 63).trim(),
+					state: bankLine.substring(63, 65).trim(),
+					city: bankLine.substring(65, 90).trim(),
+					fundsTransferStatus: bankLine.substring(90, 91).trim() === "Y",
+					fundsSettlementOnlyStatus: bankLine.substring(91,92).trim() === "S",
+					bookEntrySecuritiesTransferStatus: bankLine.substring(92, 93).trim() === "Y",
+					lastRevisedDate: bankLine.substring(93, 101).trim()
+				};
 		});
-		console.log("map loaded");
+		console.log("map loaded");		
 	});
 }
+
